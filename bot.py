48287bd990f6fa273c2d6f4844944a6d6031cfb8
Nr0ker <125687629+Nr0ker@users.noreply.github.com>
2023-02-21 18:25:53 +0100

    yes)

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
+from copy import copy
+
 import telebot
 import random
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
 
 bot = telebot.TeleBot('5988465298:AAG9vcEVtgYlDyiTloFDTc-6xLjR2245ri0')
-list_for_cartoons = ['1']
+list_for_cartoons = []
 
 def buttonses():
     markupqq = InlineKeyboardMarkup()
@@ -12,31 +14,35 @@ def buttonses():
     markupqq.add(button1, button2)
     return markupqq
 
+# def appending_lis(apendment):
+#     global list_for_cartoons
+#     list_for_cartoons.append(apendment)
+#     print(list_for_cartoons)
 @bot.message_handler(commands=['start'])
 def startFunc(message):
     bot.send_message(message.chat.id, 'Що ви хочете зробити?', reply_markup=buttonses())
     print(message.text)
 
-def appending_lis(apendment):
-    global list_for_cartoons
-    list_for_cartoons.append(apendment)
-
 @bot.message_handler(content_types=['text'])
 def get_user_info(message):
-    if message.text == "Введіть мультик який хочете додати:":
+    global list_for_cartoons
+    b = '\n'
+    if message.text:
         info = message.text
-        zminna = appending_lis(info)
-        print(zminna)
-        print(info)
+        list_for_cartoons.append(info)
+        print("Це список мультів", list_for_cartoons)
+        print("Мульт, який зараз ввели", info)
 
-        return info
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
     if call.data == 'додавання':
-        bot.send_message(call.from_user.id, 'Введіть мультик який хочете додати:')
-        print(list_for_cartoons)
+        msg = bot.send_message(call.from_user.id, 'Введіть мультик який хочете додати:')
+        bot.register_next_step_handler(msg, get_user_info)
+        # print(list_for_cartoons)
     if call.data == 'показати список':
-        bot.send_message(call.from_user.id, f'Ось весь список мультиків {list_for_cartoons}')
+        bot.send_message(call.from_user.id, str(list_for_cartoons))
+
+
 
 bot.polling(none_stop=True)
